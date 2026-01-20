515994b2e379b8530a8713c21748c7090004d412
engmohammed2502-netizen <engmohammed2502@gmail.com>
2026-01-20 17:37:17 +0200

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
+    # رابط لوحة تحكم الإدارة التي دخلت عليها سابقاً
     path('admin/', admin.site.urls),
+    
+    # هذا السطر هو الذي يربط مشروعك بمجلد المكتبة (library)
+    path('', include('library.urls')),
 ]
+
+# السماح للسيرفر بعرض ملفات الـ PDF والصور التي يرفعها الدكاترة
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
